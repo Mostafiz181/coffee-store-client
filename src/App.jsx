c00330533076fef275d7159f94082f83774a58fc
mostafizurrahman <dev.mostafizurrahman@gmail.com>
2023-05-08 15:28:56 +0600

    added 2nd class

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+      <h1 className='text-6xl text-purple-600'>Coffee-store</h1>
+
+      </div>
+      
+
+    </>
+  )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+import {createBrowserRouter,RouterProvider,} from "react-router-dom";
+  
+  
+
+import AddCoffee from '../components/AddCoffee.jsx';
+import UpdateCoffee from '../components/UpdateCoffee.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },
+  {
+    path:'AddCoffee',
+    element:<AddCoffee></AddCoffee>
+  },
+  {
+    path:'UpdateCoffee',
+    element:<UpdateCoffee></UpdateCoffee>
+
+  }
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
